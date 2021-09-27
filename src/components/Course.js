f1494bab026954845e6e605fb7c8ee89669bf0ac
Jiwon Choi <cjwon531@gmail.com>
2021-09-27 13:45:55 -0500

    add login

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getCourseTerm, hasConflict, timeParts } from '../utilities/times.js';
-import { setData } from '../utilities/firebase.js';
+import { setData, useUserState } from '../utilities/firebase.js';
 
 const getCourseNumber = course => (
   course.id.slice(1, 4)
@@ -10,7 +10,7 @@ const toggle = (x, lst) => (
   lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
 
-const getCourseMeetingData = course => {
+const getMeetingData = course => {
   const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
   const valid = !meets || timeParts(meets).days;
   if (valid) return meets;
@@ -31,6 +31,7 @@ const reschedule = async (course, meets) => {
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
   const isDisabled = !isSelected && hasConflict(course, selected);
+  const [user] = useUserState();
   const style = {
     backgroundColor: isDisabled ? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
   };
@@ -38,8 +39,8 @@ const Course = ({ course, selected, setSelected }) => {
   return (
     <div className="card m-1 p-2"
       style={style}
-      onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
-      onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
+      onClick={(isDisabled) ? null : () => setSelected(toggle(course, selected))}
+      onDoubleClick={!user ? null : () => reschedule(course, getMeetingData(course))}>
       <div className="card-body">
         <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
         <div className="card-text">{course.title}</div>
@@ -48,5 +49,4 @@ const Course = ({ course, selected, setSelected }) => {
     </div>
   );
 };
-
 export default Course;
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -2,20 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, onValue, ref, set } from 'firebase/database';
 import { useState, useEffect } from 'react'; // trackable state
+import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLsIazwkQxi3vzFpVi2VCCqUk2a60zoeA",
-  authDomain: "cs497-scheduler-b1e96.firebaseapp.com",
-  databaseURL: "https://cs497-scheduler-b1e96-default-rtdb.firebaseio.com",
-  projectId: "cs497-scheduler-b1e96",
-  storageBucket: "cs497-scheduler-b1e96.appspot.com",
-  messagingSenderId: "914330873759",
-  appId: "1:914330873759:web:f43772c5543ecc4d0057aa",
-  measurementId: "G-RVH5JW0YJ9"
+    apiKey: "AIzaSyCLsIazwkQxi3vzFpVi2VCCqUk2a60zoeA",
+    authDomain: "cs497-scheduler-b1e96.firebaseapp.com",
+    databaseURL: "https://cs497-scheduler-b1e96-default-rtdb.firebaseio.com",
+    projectId: "cs497-scheduler-b1e96",
+    storageBucket: "cs497-scheduler-b1e96.appspot.com",
+    messagingSenderId: "914330873759",
+    appId: "1:914330873759:web:f43772c5543ecc4d0057aa",
+    measurementId: "G-RVH5JW0YJ9"
 };
 
 // Initialize Firebase
@@ -23,27 +24,45 @@ const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
 
 export const useData = (path, transform) => {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
 
-  useEffect(() => {
-    const dbRef = ref(database, path);
-    return onValue(dbRef, (snapshot) => {
-      const val = snapshot.val();
-      setData(transform ? transform(val) : val);
-      setLoading(false);
-      setError(null);
-    }, (error) => {
-      setData(null);
-      setLoading(false);
-      setError(error);
-    });
-  }, [path, transform]);
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
 
-  return [data, loading, error];
+    return [data, loading, error];
 };
 
 export const setData = (path, value) => (
-  set(ref(database, path), value)
+    set(ref(database, path), value)
 );
+
+export const signInWithGoogle = () => {
+    signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+};
+
+const firebaseSignOut = () => signOut(getAuth(firebase));
+
+export { firebaseSignOut as signOut };
+
+export const useUserState = () => {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        onIdTokenChanged(getAuth(firebase), setUser);
+    }, []);
+
+    return [user];
+};
