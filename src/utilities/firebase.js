1c11c398d0cdd29330c492c8c5ca09300331807c
Jiwon Choi <cjwon531@gmail.com>
2021-09-27 12:21:04 -0500

    write to firebase

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -8,14 +8,14 @@ import { useState, useEffect } from 'react'; // trackable state
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCLsIazwkQxi3vzFpVi2VCCqUk2a60zoeA",
-    authDomain: "cs497-scheduler-b1e96.firebaseapp.com",
-    databaseURL: "https://cs497-scheduler-b1e96-default-rtdb.firebaseio.com",
-    projectId: "cs497-scheduler-b1e96",
-    storageBucket: "cs497-scheduler-b1e96.appspot.com",
-    messagingSenderId: "914330873759",
-    appId: "1:914330873759:web:f43772c5543ecc4d0057aa",
-    measurementId: "G-RVH5JW0YJ9"
+  apiKey: "AIzaSyCLsIazwkQxi3vzFpVi2VCCqUk2a60zoeA",
+  authDomain: "cs497-scheduler-b1e96.firebaseapp.com",
+  databaseURL: "https://cs497-scheduler-b1e96-default-rtdb.firebaseio.com",
+  projectId: "cs497-scheduler-b1e96",
+  storageBucket: "cs497-scheduler-b1e96.appspot.com",
+  messagingSenderId: "914330873759",
+  appId: "1:914330873759:web:f43772c5543ecc4d0057aa",
+  measurementId: "G-RVH5JW0YJ9"
 };
 
 // Initialize Firebase
@@ -23,23 +23,27 @@ const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
 
 export const useData = (path, transform) => {
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState();
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
-    useEffect(() => {
-        const dbRef = ref(database, path);
-        return onValue(dbRef, (snapshot) => {
-            const val = snapshot.val();
-            setData(transform ? transform(val) : val);
-            setLoading(false);
-            setError(null);
-        }, (error) => {
-            setData(null);
-            setLoading(false);
-            setError(error);
-        });
-    }, [path, transform]);
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    return onValue(dbRef, (snapshot) => {
+      const val = snapshot.val();
+      setData(transform ? transform(val) : val);
+      setLoading(false);
+      setError(null);
+    }, (error) => {
+      setData(null);
+      setLoading(false);
+      setError(error);
+    });
+  }, [path, transform]);
 
-    return [data, loading, error];
+  return [data, loading, error];
 };
+
+export const setData = (path, value) => (
+  set(ref(database, path), value)
+);
