e9ebe5d5357cdb78ffdaa2615cb0c17ae676ab9a
Jiwon Choi <cjwon531@gmail.com>
2021-09-27 00:50:28 -0500

    read from firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useState, useEffect } from 'react'; // trackable state
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCLsIazwkQxi3vzFpVi2VCCqUk2a60zoeA",
+    authDomain: "cs497-scheduler-b1e96.firebaseapp.com",
+    databaseURL: "https://cs497-scheduler-b1e96-default-rtdb.firebaseio.com",
+    projectId: "cs497-scheduler-b1e96",
+    storageBucket: "cs497-scheduler-b1e96.appspot.com",
+    messagingSenderId: "914330873759",
+    appId: "1:914330873759:web:f43772c5543ecc4d0057aa",
+    measurementId: "G-RVH5JW0YJ9"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+
+    return [data, loading, error];
+};
